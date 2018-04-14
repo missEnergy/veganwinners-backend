8e7d3af76abf455e8ad9c2006a30835ad78afd8c
missEnergy <everseput@hotmail.com>
2018-04-14 17:03:13 +0200

    tried options on endpoints

--- veganwinners-API/app/recipes.py
+++ veganwinners-API/app/recipes.py
@@ -8,7 +8,7 @@ import json
 recipes_blueprint = Blueprint('recipes', __name__)
 
 
-@recipes_blueprint.route('/<limit>')
+@recipes_blueprint.route('/<limit>', methods=['GET', 'OPTIONS'])
 def get_all_recipes(limit):
     recipes = Recipe.query \
         .limit(limit)
@@ -25,7 +25,7 @@ def get_all_recipes(limit):
     return return_result(data=data)
 
 
-@recipes_blueprint.route('/one/<id>')
+@recipes_blueprint.route('/one/<id>', methods=['GET', 'OPTIONS'])
 def get_recipe_for_id(id):
     try:
         recipe = Recipe.query.filter(Recipe.id == id)[0]
