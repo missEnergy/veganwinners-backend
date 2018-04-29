cb562b96f6586ba3addb984d3ef667fe9e6f2091
missEnergy <everseput@hotmail.com>
2018-04-29 18:19:31 +0200

    first try

--- veganwinners-API/app/__init__.py
+++ veganwinners-API/app/__init__.py
@@ -4,16 +4,19 @@ from app.database import db_session, init_db
 from app.recipes import recipes_blueprint
 from app import config
 from flask_cors import CORS
+from app.custom_logger import setup_logger
+from app.helpers import return_result
 
 logger = logging.getLogger()
 logger.handlers = []
+setup_logger(logger)
 
 app = Flask(__name__)
 CORS(app)
 
 init_db()
 
-app.register_blueprint(recipes_blueprint, url_prefix='/recipes')
+app.register_blueprint(recipes_blueprint, url_prefix='/api/recipes')
 
 @app.errorhandler(500)
 def server_error(e):
--- veganwinners-API/app/recipes.py
+++ veganwinners-API/app/recipes.py
@@ -7,8 +7,7 @@ import json
 
 recipes_blueprint = Blueprint('recipes', __name__)
 
-
-@recipes_blueprint.route('/<limit>', methods=['GET', 'OPTIONS'])
+@recipes_blueprint.route('/<limit>', methods=['GET'])
 def get_all_recipes(limit):
     recipes = Recipe.query \
         .limit(limit)
@@ -25,7 +24,7 @@ def get_all_recipes(limit):
     return return_result(data=data)
 
 
-@recipes_blueprint.route('/one/<id>', methods=['GET', 'OPTIONS'])
+@recipes_blueprint.route('/one/<id>', methods=['GET'])
 def get_recipe_for_id(id):
     try:
         recipe = Recipe.query.filter(Recipe.id == id)[0]
